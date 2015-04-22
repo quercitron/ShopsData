a057971ebf99daad501e7f49a38cdd604fbb4d3f
quercitron <oldquercitron@yandex.ru>
2015-04-22 20:35:09 +0300

    Add PostgreDAL project

---
+++ PostgreDAL/IShopsDataStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PostgreDAL
+{
+    public interface IShopsDataStore
+    {
+        List<string> GetProductTypes();
+
+        void AddProductType(string productType);
+    }
+}
