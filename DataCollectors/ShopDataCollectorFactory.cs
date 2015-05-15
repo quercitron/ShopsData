2f9981037fa0081f75634ee4f4101a84607982bb
quercitron <oldquercitron@yandex.ru>
2015-05-15 20:22:48 +0300

    Add GeneralDataCollector

---
+++ DataCollectors/ShopDataCollectorFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using DataCollectorCore;
+
+namespace DataCollectors
+{
+    public class ShopDataCollectorFactory
+    {
+        public IShopDataCollector Create(string sourceName)
+        {
+            IShopDataCollector shopDataCollector;
+            switch (sourceName.ToLower())
+            {
+                case "dns":
+                    shopDataCollector = new DnsDataCollector();
+                    break;
+                case "citilink":
+                    shopDataCollector = new CitilinkDataCollector();
+                    break;
+                default:
+                    var message = string.Format("Data source '{0}' is not supported", sourceName);
+                    throw new NotSupportedException(message);
+            }
+            return shopDataCollector;
+        }
+    }
+}
---
+++ TestDataCollector/GeneralDataCollector.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+using DataCollectorCore.DataObjects;
+
+using DataCollectors;
+
+using PostgreDAL;
+
+namespace TestDataCollector
+{
+    public class GeneralDataCollector
+    {
+        private readonly ShopDataCollectorFactory _shopDataCollectorFactory = new ShopDataCollectorFactory();
+
+        private readonly IShopsDataStore _dataStore = new ShopsDataStore("shopsdata");
+
+        public IEnumerable<ProductRecord> GetData()
+        {
+            var records = new List<ProductRecord>();
+
+            var sources = _dataStore.GetDataSources();
+
+            foreach (var dataSource in sources)
+            {
+                records.AddRange(GetDataFromSource(dataSource));
+            }
+
+            return records;
+        }
+
+        private List<ProductRecord> GetDataFromSource(DataSource dataSource)
+        {
+            var records = new List<ProductRecord>();
+
+            var dataCollector = _shopDataCollectorFactory.Create(dataSource.Name);
+
+            var productTypes = _dataStore.GetProductTypes();
+            foreach (var productType in productTypes)
+            {
+                var shopDataResult = dataCollector.GetShopData(productType.Name);
+                if (shopDataResult.Success)
+                {
+                    records.AddRange(shopDataResult.Products);
+                }
+                // todo: log message
+            }
+
+            foreach (var productRecord in records)
+            {
+                // todo: add source id
+                //productRecord.
+            }
+
+            return records;
+        }
+    }
+}
