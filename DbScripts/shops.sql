5c4320f656208cddc4c117c1010dfe73fe3b4d52
quercitron <oldquercitron@yandex.ru>
2015-08-26 21:00:48 +0300

    Add auto increment for primary key

--- DbScripts/shops.sql
+++ DbScripts/shops.sql
@@ -2,10 +2,6 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.4.1
--- Dumped by pg_dump version 9.4.1
--- Started on 2015-08-25 13:18:12
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
@@ -14,7 +10,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 182 (class 3079 OID 11855)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -22,8 +17,6 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2060 (class 0 OID 0)
--- Dependencies: 182
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
 --
 
@@ -35,7 +28,6 @@ SET search_path = public, pg_catalog;
 SET default_with_oids = false;
 
 --
--- TOC entry 172 (class 1259 OID 16527)
 -- Name: productrecord; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -55,7 +47,6 @@ CREATE TABLE productrecord (
 
 
 --
--- TOC entry 173 (class 1259 OID 16533)
 -- Name: ProductRecord_ProductRecordId_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -68,8 +59,6 @@ CREATE SEQUENCE "ProductRecord_ProductRecordId_seq"
 
 
 --
--- TOC entry 2061 (class 0 OID 0)
--- Dependencies: 173
 -- Name: ProductRecord_ProductRecordId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -77,7 +66,6 @@ ALTER SEQUENCE "ProductRecord_ProductRecordId_seq" OWNED BY productrecord.produc
 
 
 --
--- TOC entry 174 (class 1259 OID 16535)
 -- Name: producttype; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -88,7 +76,6 @@ CREATE TABLE producttype (
 
 
 --
--- TOC entry 175 (class 1259 OID 16541)
 -- Name: ProductType_TypeId_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -101,8 +88,6 @@ CREATE SEQUENCE "ProductType_TypeId_seq"
 
 
 --
--- TOC entry 2062 (class 0 OID 0)
--- Dependencies: 175
 -- Name: ProductType_TypeId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -110,7 +95,6 @@ ALTER SEQUENCE "ProductType_TypeId_seq" OWNED BY producttype.producttypeid;
 
 
 --
--- TOC entry 176 (class 1259 OID 16543)
 -- Name: product; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -122,7 +106,6 @@ CREATE TABLE product (
 
 
 --
--- TOC entry 177 (class 1259 OID 16549)
 -- Name: Product_ProductId_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -135,8 +118,6 @@ CREATE SEQUENCE "Product_ProductId_seq"
 
 
 --
--- TOC entry 2063 (class 0 OID 0)
--- Dependencies: 177
 -- Name: Product_ProductId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -144,7 +125,6 @@ ALTER SEQUENCE "Product_ProductId_seq" OWNED BY product.productid;
 
 
 --
--- TOC entry 178 (class 1259 OID 16551)
 -- Name: datasource; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -155,7 +135,6 @@ CREATE TABLE datasource (
 
 
 --
--- TOC entry 179 (class 1259 OID 16557)
 -- Name: datasource_datasourceId_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -168,8 +147,6 @@ CREATE SEQUENCE "datasource_datasourceId_seq"
 
 
 --
--- TOC entry 2064 (class 0 OID 0)
--- Dependencies: 179
 -- Name: datasource_datasourceId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -177,7 +154,6 @@ ALTER SEQUENCE "datasource_datasourceId_seq" OWNED BY datasource.datasourceid;
 
 
 --
--- TOC entry 180 (class 1259 OID 16559)
 -- Name: location; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -188,7 +164,25 @@ CREATE TABLE location (
 
 
 --
--- TOC entry 181 (class 1259 OID 16565)
+-- Name: location_locationid_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE location_locationid_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: location_locationid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE location_locationid_seq OWNED BY location.locationid;
+
+
+--
 -- Name: sourceproduct; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -203,7 +197,25 @@ CREATE TABLE sourceproduct (
 
 
 --
--- TOC entry 1916 (class 2604 OID 16571)
+-- Name: sourceproduct_sourceproductid_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE sourceproduct_sourceproductid_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: sourceproduct_sourceproductid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE sourceproduct_sourceproductid_seq OWNED BY sourceproduct.sourceproductid;
+
+
+--
 -- Name: datasourceid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -211,7 +223,6 @@ ALTER TABLE ONLY datasource ALTER COLUMN datasourceid SET DEFAULT nextval('"data
 
 
 --
--- TOC entry 1915 (class 2604 OID 16572)
 -- Name: productid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -219,7 +230,6 @@ ALTER TABLE ONLY product ALTER COLUMN productid SET DEFAULT nextval('"Product_Pr
 
 
 --
--- TOC entry 1913 (class 2604 OID 16573)
 -- Name: productrecordid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -227,7 +237,6 @@ ALTER TABLE ONLY productrecord ALTER COLUMN productrecordid SET DEFAULT nextval(
 
 
 --
--- TOC entry 1914 (class 2604 OID 16574)
 -- Name: producttypeid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -235,7 +244,6 @@ ALTER TABLE ONLY producttype ALTER COLUMN producttypeid SET DEFAULT nextval('"Pr
 
 
 --
--- TOC entry 1918 (class 2606 OID 16576)
 -- Name: ProductRecord_PK_ProductRecordId; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -244,7 +252,6 @@ ALTER TABLE ONLY productrecord
 
 
 --
--- TOC entry 1922 (class 2606 OID 16578)
 -- Name: ProductType_PK_ProductTypeId; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -253,7 +260,6 @@ ALTER TABLE ONLY producttype
 
 
 --
--- TOC entry 1926 (class 2606 OID 16580)
 -- Name: Product_PK_ProductId; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -262,7 +268,6 @@ ALTER TABLE ONLY product
 
 
 --
--- TOC entry 1928 (class 2606 OID 16582)
 -- Name: datasource_PK_datasourceId; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -271,7 +276,6 @@ ALTER TABLE ONLY datasource
 
 
 --
--- TOC entry 1930 (class 2606 OID 16584)
 -- Name: datasource_unique_name; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -280,7 +284,6 @@ ALTER TABLE ONLY datasource
 
 
 --
--- TOC entry 1932 (class 2606 OID 16586)
 -- Name: location_pk_locationid; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -289,7 +292,6 @@ ALTER TABLE ONLY location
 
 
 --
--- TOC entry 1934 (class 2606 OID 16588)
 -- Name: location_unique_name; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -298,7 +300,6 @@ ALTER TABLE ONLY location
 
 
 --
--- TOC entry 1924 (class 2606 OID 16590)
 -- Name: producttype_unique_name; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -307,7 +308,6 @@ ALTER TABLE ONLY producttype
 
 
 --
--- TOC entry 1937 (class 2606 OID 16592)
 -- Name: sourceproduct_pk_sourceproductid; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -316,7 +316,6 @@ ALTER TABLE ONLY sourceproduct
 
 
 --
--- TOC entry 1939 (class 2606 OID 16594)
 -- Name: sourceproduct_unique_datasourceid_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -325,7 +324,6 @@ ALTER TABLE ONLY sourceproduct
 
 
 --
--- TOC entry 1919 (class 1259 OID 16595)
 -- Name: fki_productrecord_fk_locationid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -333,7 +331,6 @@ CREATE INDEX fki_productrecord_fk_locationid ON productrecord USING btree (locat
 
 
 --
--- TOC entry 1920 (class 1259 OID 16596)
 -- Name: fki_productrecord_fk_sourceproductid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -341,7 +338,6 @@ CREATE INDEX fki_productrecord_fk_sourceproductid ON productrecord USING btree (
 
 
 --
--- TOC entry 1935 (class 1259 OID 16622)
 -- Name: fki_sourceproduct_fk_productid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -349,7 +345,6 @@ CREATE INDEX fki_sourceproduct_fk_productid ON sourceproduct USING btree (produc
 
 
 --
--- TOC entry 1942 (class 2606 OID 16597)
 -- Name: Product_FK_ProductTypeId; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -358,7 +353,6 @@ ALTER TABLE ONLY product
 
 
 --
--- TOC entry 1940 (class 2606 OID 16602)
 -- Name: productrecord_fk_locationid; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -367,7 +361,6 @@ ALTER TABLE ONLY productrecord
 
 
 --
--- TOC entry 1941 (class 2606 OID 16607)
 -- Name: productrecord_fk_sourceproductid; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -376,7 +369,6 @@ ALTER TABLE ONLY productrecord
 
 
 --
--- TOC entry 1943 (class 2606 OID 16612)
 -- Name: sourceproduct_fk_datasourceid; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -385,15 +377,12 @@ ALTER TABLE ONLY sourceproduct
 
 
 --
--- TOC entry 1944 (class 2606 OID 16617)
 -- Name: sourceproduct_fk_productid; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY sourceproduct
     ADD CONSTRAINT sourceproduct_fk_productid FOREIGN KEY (productid) REFERENCES product(productid);
 
-
--- Completed on 2015-08-25 13:18:12
 
 --
 -- PostgreSQL database dump complete
